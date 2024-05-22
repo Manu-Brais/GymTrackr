d32595d0533cd2597961bc3b9a1263f18292579f
braisoncrece <braisoncrece@gmail.com>
2024-05-22 13:39:33 +0200

    refactor(authentication): remove unused arguments from SignUp mutation
    Removed name, surname, phone, and address arguments from the SignUp
    mutation. Updated the set_authenticatable method to reflect these
    changes. This simplifies the sign-up process by minimizing the required
    user information.

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,7 +2,7 @@ module Types
   class UserType < BaseObject
     field :id, ID, null: false
     field :email, String, null: false
-    field :password, String, null: false
+    field :password, String, null: true
 
     field :authenticatable, Types::AuthenticatableType, null: false, method: :authenticatable
   end
