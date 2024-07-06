d3071480cc1b792903364baee836a2902c1d82bb
BraisonCrece <104019638+BraisonCrece@users.noreply.github.com>
2024-07-06 17:16:05 +0200

    feat(graphql): add delete and update exercise mutations (#40)
    Added fields for deleting and updating exercises in MutationType to
    support new functionalities for exercise management by coaches.

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -5,6 +5,8 @@ module Types
     field :signup, mutation: Mutations::Public::Authentication::SignUp
     field :login, mutation: Mutations::Public::Authentication::LogIn
     field :create_exercise, mutation: Mutations::Protected::Coach::Exercises::CreateExercise
+    field :delete_exercise, mutation: Mutations::Protected::Coach::Exercises::DeleteExercise
+    field :update_exercise, mutation: Mutations::Protected::Coach::Exercises::UpdateExercise
     field :update_user_data, mutation: Mutations::Protected::User::UpdateUserData
   end
 end
